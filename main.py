d2affb4020a0959d19d602a95340559f516e0b46
SRshohan <srshohan02@gmail.com>
2023-08-12 23:33:11 -0400

    Added some comments

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 def translate(phrase):
     translation = ""
-    for letter in phrase:
-        if letter in "AEIOUaeiou":
-            translation = translation + "g"
+    for letter in phrase:                       #Loops helps to go throgh each letter of phrase
+        if letter in "AEIOUaeiou":              # After the loop, it checks through if the letter in ""AEIOUaeiou""
+            translation = translation + "g"     # If it is ,It takes each letter and check for the vowel ( Vowel detector). Then, replace with "g".
         else:
             translation = translation + letter
     return translation
